b9e254b8fc6c338a21b5c1638810306ed18d9fe1
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2020-07-30 13:14:42 +0200

    added all events worth using

--- src/app/events.ts
+++ src/app/events.ts
@@ -47,51 +47,222 @@ export class Events {
       });
     }
 
+    //#region channel events
+    if (config.checkFunctionanlity('channelCreate')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('channelCreate')`);
+      bot.client.on('channelCreate', async () => {
+        // This event triggers when a new channel is created in a guild.
+      });
+    }
+    if (config.checkFunctionanlity('channelCreate')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('channelCreate')`);
+      bot.client.on('channelDelete', async () => {
+        // This event triggers when a channel is deleted in a guild.
+      });
+    }
+    if (config.checkFunctionanlity('channelCreate')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('channelPinsUpdate')`);
+      bot.client.on('channelPinsUpdate', async () => {
+        // This event triggers when a message has heen pinned or removed form pins in a channel.
+      });
+    }
+    if (config.checkFunctionanlity('channelCreate')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('channelUpdate')`);
+      bot.client.on('channelUpdate', async () => {
+        // This event triggers when a channel is modified in a guild.
+      });
+    }
+    //#endregion
+
+    //#region guild events
     if (config.checkFunctionanlity('guildMemberAdd')) {
       console.debug(`attachEventListeners() -- checkFunctionanlity('guildMemberAdd')`);
       bot.client.on('guildMemberAdd', async (member: GuildMember) => {
+        // This event triggers when a member has joined a guild.
         await events.guildMemberAddEvent(config, member);
       });
     }
-
     if (config.checkFunctionanlity('guildMemberRemove')) {
       console.debug(`attachEventListeners() -- checkFunctionanlity('guildMemberRemove')`);
       bot.client.on('guildMemberRemove', async (member: GuildMember) => {
+        // This event triggers when a member has left a guild.
         await events.guildMemberRemoveEvent(config, member);
       });
     }
-
+    if (config.checkFunctionanlity('guildMemberAvailable')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('guildMemberAvailable')`);
+      bot.client.on('guildMemberAvailable', async () => {
+        // This event triggers when a guild member has become available.
+      });
+    }
+    if (config.checkFunctionanlity('guildMemberSpeaking')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('guildMemberSpeaking')`);
+      bot.client.on('guildMemberSpeaking', async () => {
+        // This event triggers when a guild member has started speaking.
+      });
+    }
+    if (config.checkFunctionanlity('guildMemberUpdate')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('guildMemberUpdate')`);
+      bot.client.on('guildMemberUpdate', async () => {
+        // This event triggers when a guild member has been updated.
+      });
+    }
+    if (config.checkFunctionanlity('guildMembersChunk')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('guildMembersChunk')`);
+      bot.client.on('guildMembersChunk', async () => {
+        // This event triggers when a guild member chunk of data has been send.
+      });
+    }
+    if (config.checkFunctionanlity('guildIntegrationsUpdate')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('guildIntegrationsUpdate')`);
+      bot.client.on('guildIntegrationsUpdate', async () => {
+        // This event triggers, that's all i know.
+      });
+    }
+    if (config.checkFunctionanlity('guildUnavailable')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('guildUnavailable')`);
+      bot.client.on('guildUnavailable', async () => {
+        // This event triggers when a guild has become unavailable.
+      });
+    }
     if (config.checkFunctionanlity('guildBanAdd')) {
       console.debug(`attachEventListeners() -- checkFunctionanlity('guildBanAdd')`);
       bot.client.on('guildBanAdd', async (guild: Guild, user: User) => {
+        // This event triggers when a ban has been added in a guild.
         await events.guildBanAddEvent(config, guild, user);
       });
     }
-
     if (config.checkFunctionanlity('guildBanRemove')) {
       console.debug(`attachEventListeners() -- checkFunctionanlity('guildBanRemove')`);
       bot.client.on('guildBanRemove', async (guild: Guild, user: User) => {
+        // This event triggers when a ban has been removed from a guild.
         await events.guildBanRemoveEvent(config, guild, user);
       });
     }
-
     if (config.checkFunctionanlity('guildCreate')) {
       console.debug(`attachEventListeners() -- checkFunctionanlity('guildCreate')`);
       bot.client.on('guildCreate', async (guild: Guild) => {
         // This event triggers when the bot joins a guild.
-        // console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-        // client.user.setActivity(`Serving ${client.guilds.size} servers`);
+        console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
         await bot.client.user.setActivity(`${bot.client.guilds.cache.size} Guilds`, { type: "WATCHING" });
       });
     }
-
     if (config.checkFunctionanlity('guildDelete')) {
       console.debug(`attachEventListeners() -- checkFunctionanlity('guildDelete')`);
       bot.client.on('guildDelete', async (guild: Guild) => {
         // this event triggers when the bot is removed from a guild.
-        // console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-        // client.user.setActivity(`Serving ${client.guilds.size} servers`);
+        console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
         await bot.client.user.setActivity(`${bot.client.guilds.cache.size} Guilds`, { type: "WATCHING" });
+      });
+    }
+    //#endregion
+
+    if (config.checkFunctionanlity('emojiCreate')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('emojiCreate')`);
+      bot.client.on('emojiCreate', async () => {
+        // This event triggers when an emoji has been added to a guild.
+      });
+    }
+    if (config.checkFunctionanlity('emojiDelete')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('emojiDelete')`);
+      bot.client.on('emojiDelete', async () => {
+        // This event triggers when an emoji has been removed from a guild
+      });
+    }
+    if (config.checkFunctionanlity('emojiUpdate')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('emojiUpdate')`);
+      bot.client.on('emojiUpdate', async () => {
+        // This event triggers when an emoji has been updated in a guild.
+      });
+    }
+    if (config.checkFunctionanlity('disconnect')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('disconnect')`);
+      bot.client.on('disconnect', async () => {
+        // This event triggers when the bot has disconnected.
+        console.log('disconnected');
+      });
+    }
+    if (config.checkFunctionanlity('inviteCreate')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('inviteCreate')`);
+      bot.client.on('inviteCreate', async () => {
+        // This event triggers when a guild invite hes been created.
+      });
+    }
+    if (config.checkFunctionanlity('inviteDelete')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('inviteDelete')`);
+      bot.client.on('inviteDelete', async () => {
+        // This event triggers when a guild invite has been deleted.
+      });
+    }
+    //#region message event triggers
+    if (config.checkFunctionanlity('message')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('message')`);
+      bot.client.on('message', async () => {
+        // This event triggers when a guild invite has been deleted.
+      });
+    }
+    if (config.checkFunctionanlity('messageDelete')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('messageDelete')`);
+      bot.client.on('messageDelete', async () => {
+        // This event triggers when a message has been deleted.
+      });
+    }
+    if (config.checkFunctionanlity('messageDeleteBulk')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('messageDeleteBulk')`);
+      bot.client.on('messageDeleteBulk', async () => {
+        // This event triggers when a bulk delete of messages has started.
+      });
+    }
+    if (config.checkFunctionanlity('messageReactionAdd')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('messageReactionAdd')`);
+      bot.client.on('messageReactionAdd', async () => {
+        // This event triggers when a reaction has been added to a message.
+      });
+    }
+    if (config.checkFunctionanlity('messageReactionRemove')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('messageReactionRemove')`);
+      bot.client.on('messageReactionRemove', async () => {
+        // This event triggers when a reaction has been removed from a message.
+      });
+    }
+    if (config.checkFunctionanlity('messageReactionRemoveAll')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('messageReactionRemoveAll')`);
+      bot.client.on('messageReactionRemoveAll', async () => {
+        // This event triggers when all reactions have been removed from a message.
+      });
+    }
+    if (config.checkFunctionanlity('messageReactionRemoveEmoji')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('messageReactionRemoveEmoji')`);
+      bot.client.on('messageReactionRemoveEmoji', async () => {
+        // This event triggers a emoji has been removed as reaction from a message.
+      });
+    }
+    //#endregion
+    //#region role events
+    if (config.checkFunctionanlity('roleCreate')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('roleCreate')`);
+      bot.client.on('roleCreate', async () => {
+        // This event triggers when a guild role has been created.
+      });
+    }
+    if (config.checkFunctionanlity('roleUpdate')) {
+      console.debug(`attachEventListeners() -- checkFunctionality('roleUpdate')`);
+      bot.client.on('roleUpdate', async () => {
+        // This event triggers when a guild role has been updated.
+      });
+    }
+    if (config.checkFunctionanlity('roleDelete')) {
+      console.debug(`attachEventListeners() -- checkFunctionality('roleDelete')`);
+      bot.client.on('roleDelete', async () => {
+        // This event triggers when a guild role has been updated.
+      });
+    }
+    //#endregion
+
+    if (config.checkFunctionanlity('rateLimit')) {
+      console.debug(`attachEventListeners() -- checkFunctionanlity('rateLimit')`);
+      bot.client.on('rateLimit', async () => {
+        // This event triggers the bot has been rate limited.
       });
     }
 

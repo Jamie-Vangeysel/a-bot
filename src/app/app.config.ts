f16af8307b78e8ac8df13cb5dd08679e123f7dae
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2020-10-09 12:52:07 +0200

    removed token from config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { IBotConfig } from "./models/bot-config";
 
 export const DefaultConfig: IBotConfig = {
   googleApiKey: null,
-  token: 'NTI5MjAxMDM1NDQ1NjY1Nzky.DwtbWw.fxqqcBD_-EuCbd3hDKBwLzJbsjI',
+  token: null,
   prefix: ';',
   dbConnection: 'mongodb+srv://alastor:nuwqen-4jaqNu-fuzkoh@cluster0-1z5qu.mongodb.net/test?retryWrites=true&w=majority',
   mode: 'standalone',
